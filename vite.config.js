0c5b822d8966ec2f36f0147cf5da1c8ef721d597
afont_disgrafic <afont@disgrafic.com>
2025-07-01 06:02:28 +0200

    update deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',  // Sustituye esto con el nombre de tu repositorio
   plugins: [react()],
+  base: '/', // Como tu repo se llama fontalbert.github.io, la base es '/'
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
